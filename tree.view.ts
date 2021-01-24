5b19bf5e5a480a5934383204bf777b007e3128b9
jin <jin@hyoo.ru>
2021-01-24 19:14:08 +0800

    Custom pipelines

--- tree.view.ts
+++ tree.view.ts
@@ -2,33 +2,72 @@ namespace $.$$ {
 
 	export class $hyoo_tree extends $.$hyoo_tree {
 
-		compile() {
-			return this.$.$mol_state_arg.value( 'compile' ) === ''
+		@ $mol_mem
+		pipeline( next?: string[] ) {
+			return ( this.$.$mol_state_arg.value( 'pipeline', next && next.join( '~' ) )
+				?? '$mol_tree2_from_string' ).split( '~' ).filter( Boolean )
 		}
 
+		@ $mol_mem
 		pages() {
 			return [
 				this.Source() ,
-				... this.compile() ? [ this.Result() ] : []
+				... this.pipeline().map( ( transform, index )=> this.Result( index ) ),
+				this.Result( this.pipeline().length ),
+			]
+		}
+
+		@ $mol_mem_key
+		result_head( index: number ) {
+			return [
+				this.Transform( index ),
+				... this.pipeline()[ index ] ? [ this.Close( index ) ] : [],
 			]
 		}
 
 		@ $mol_mem
-		compiled() {
-			const data = this.source()
-			const node = $mol_tree2.fromString( data , $mol_span.entire('view.tree', data.length) )
-
-			return this.$.$mol_view_tree2_ts_compile( node )
-		}
-
-		result() {
-			return ''
-			+ '# view.tree.ts\n\n' + this.compiled().script.replace( /^/gm , '\t' ) + '\n'
-			+ '# view.tree.locale=en.json\n\n' + JSON.stringify( this.compiled().locales , null , '\t' ).replace( /^/gm , '\t' )
-		}
-
 		source( next? : string ) {
 			return this.$.$mol_state_arg.value( 'source' , next ) ?? this.$.$mol_fetch.text( 'hyoo/tree/tree.view.tree' )
+		}
+
+		@ $mol_mem_key
+		transform( index: number, next?: string ) {
+			let pipeline = this.pipeline()
+			if( next ) pipeline = this.pipeline([
+				... pipeline.slice( 0, index ),
+				next,
+				... pipeline.slice( index + 1 ),
+			])
+			return pipeline[ index ] ?? null
+		}
+
+		@ $mol_mem_key
+		result( index: number ): string | $mol_tree2 {
+			
+			const func = this.pipeline()[ index ]
+			if( !func ) return ''
+
+			return this.$[ func ](
+				index ? this.result( index - 1 ) : this.source(),
+				index ? undefined : $mol_span.entire( 'source1', this.source() )
+			)
+
+		}
+
+		@ $mol_mem_key
+		result_text( index: number ): string {
+			const res = $mol_try( ()=> this.result( index ) )
+			if( res instanceof Promise ) $mol_fail_hidden( res )
+			if( typeof res === 'string' ) return res
+			if( !Reflect.getPrototypeOf( Reflect.getPrototypeOf( res ) ) ) return JSON.stringify( res, null, '\t' )
+			return String( res )
+		}
+
+		close( index: number ) {
+			this.pipeline([
+				... this.pipeline().slice( 0, index ),
+				... this.pipeline().slice( index + 1 ),
+			])
 		}
 
 	}
