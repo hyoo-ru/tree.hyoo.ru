a0ed379a935cd55838f856604d5ecff8a1cc0d38
nin-jin <nin-jin@users.noreply.github.com>
2021-03-07 11:20:43 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2761,11 +2761,10 @@ declare namespace $.$$ {
 
 declare namespace $ {
     class $mol_text_code_row extends $mol_paragraph {
-        attr(): {
-            mol_text_code_row_numb: number;
-        };
         text(): string;
         minimal_height(): number;
+        numb_showed(): boolean;
+        Numb(): $mol_view;
         Token(id: any): $mol_text_code_token;
         Token_link(id: any): $mol_text_code_token_link;
         numb(): number;
@@ -2786,7 +2785,7 @@ declare namespace $.$$ {
             found: string;
             chunks: string[];
         }[];
-        sub(): $mol_text_code_token[];
+        sub(): $mol_view[];
         row_content(path: number[]): $mol_text_code_token[];
         Token(path: number[]): $mol_text_code_token;
         token_type(path: number[]): string;
@@ -2801,6 +2800,7 @@ declare namespace $ {
         text(): string;
         text_lines(): readonly string[];
         Row(id: any): $$.$mol_text_code_row;
+        sidebar_showed(): boolean;
         row_numb(id: any): number;
         row_text(id: any): string;
         highlight(): string;
@@ -2823,6 +2823,7 @@ declare namespace $ {
     class $mol_textarea extends $mol_view {
         attr(): {
             mol_textarea_clickable: any;
+            mol_textarea_sidebar_showed: boolean;
         };
         event(): {
             keydown: (event?: any) => any;
@@ -2830,6 +2831,7 @@ declare namespace $ {
         };
         sub(): readonly any[];
         clickable(val?: any): any;
+        sidebar_showed(): boolean;
         press(event?: any): any;
         hover(event?: any): any;
         value(val?: any): any;
@@ -2837,6 +2839,7 @@ declare namespace $ {
         enabled(): boolean;
         length_max(): number;
         Edit(): $$.$mol_string;
+        row_numb(index: any): number;
         View(): $$.$mol_text_code;
     }
 }
@@ -2850,6 +2853,7 @@ declare namespace $.$$ {
         indent_dec(): void;
         hover(event: PointerEvent): void;
         press(event: KeyboardEvent): void;
+        row_numb(index: number): number;
     }
 }
 
--- web.d.ts
+++ web.d.ts
@@ -2774,11 +2774,10 @@ declare namespace $.$$ {
 
 declare namespace $ {
     class $mol_text_code_row extends $mol_paragraph {
-        attr(): {
-            mol_text_code_row_numb: number;
-        };
         text(): string;
         minimal_height(): number;
+        numb_showed(): boolean;
+        Numb(): $mol_view;
         Token(id: any): $mol_text_code_token;
         Token_link(id: any): $mol_text_code_token_link;
         numb(): number;
@@ -2799,7 +2798,7 @@ declare namespace $.$$ {
             found: string;
             chunks: string[];
         }[];
-        sub(): $mol_text_code_token[];
+        sub(): $mol_view[];
         row_content(path: number[]): $mol_text_code_token[];
         Token(path: number[]): $mol_text_code_token;
         token_type(path: number[]): string;
@@ -2814,6 +2813,7 @@ declare namespace $ {
         text(): string;
         text_lines(): readonly string[];
         Row(id: any): $$.$mol_text_code_row;
+        sidebar_showed(): boolean;
         row_numb(id: any): number;
         row_text(id: any): string;
         highlight(): string;
@@ -2836,6 +2836,7 @@ declare namespace $ {
     class $mol_textarea extends $mol_view {
         attr(): {
             mol_textarea_clickable: any;
+            mol_textarea_sidebar_showed: boolean;
         };
         event(): {
             keydown: (event?: any) => any;
@@ -2843,6 +2844,7 @@ declare namespace $ {
         };
         sub(): readonly any[];
         clickable(val?: any): any;
+        sidebar_showed(): boolean;
         press(event?: any): any;
         hover(event?: any): any;
         value(val?: any): any;
@@ -2850,6 +2852,7 @@ declare namespace $ {
         enabled(): boolean;
         length_max(): number;
         Edit(): $$.$mol_string;
+        row_numb(index: any): number;
         View(): $$.$mol_text_code;
     }
 }
@@ -2863,6 +2866,7 @@ declare namespace $.$$ {
         indent_dec(): void;
         hover(event: PointerEvent): void;
         press(event: KeyboardEvent): void;
+        row_numb(index: number): number;
     }
 }
 
