762952cfd8afc01b0348e6ae8661f0d503fb7e3c
nin-jin <nin-jin@users.noreply.github.com>
2021-03-28 19:14:17 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -10734,7 +10734,7 @@ var $;
                 },
                 "$mol_js_eval": {
                     input: "text",
-                    output: "any"
+                    output: ""
                 }
             };
         }
@@ -11094,7 +11094,9 @@ var $;
                 const map = this.transform_map();
                 const pipeline = this.pipeline();
                 const last = pipeline[pipeline.length - 1];
-                const type = last ? map[last].output.split('.').reverse() : ['text'];
+                const type = last ? map[last].output.split('.').filter(Boolean).reverse() : ['text'];
+                if (!type.length)
+                    return Object.keys(map);
                 return Object.keys(map).filter(id => {
                     const diff = $.$mol_diff_path(type, map[id].input.split('.').reverse());
                     if (!diff.prefix.length)
