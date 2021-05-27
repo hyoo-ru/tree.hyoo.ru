5c4d6de7070561fb441483758c5c2359358508c7
nin-jin <nin-jin@users.noreply.github.com>
2021-05-27 07:42:15 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -3044,6 +3044,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    var _a;
     class $mol_span extends $.$mol_object2 {
         constructor(uri, source, row, col, length) {
             super();
@@ -3052,7 +3053,7 @@ var $;
             this.row = row;
             this.col = col;
             this.length = length;
-            this[Symbol.toStringTag] = `${this.uri}#${this.row}:${this.col}/${this.length}`;
+            this[_a] = `${this.uri}#${this.row}:${this.col}/${this.length}`;
         }
         static begin(uri, source = '') {
             return new $mol_span(uri, source, 1, 1, 0);
@@ -3098,6 +3099,7 @@ var $;
             return this.span(this.row, this.col + begin, end - begin);
         }
     }
+    _a = Symbol.toStringTag;
     $mol_span.unknown = $mol_span.begin('unknown');
     $.$mol_span = $mol_span;
 })($ || ($ = {}));
