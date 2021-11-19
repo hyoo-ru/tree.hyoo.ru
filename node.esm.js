315e68571a0ec212e2bde0a135f45c939996541b
nin-jin <nin-jin@users.noreply.github.com>
2021-11-19 07:06:59 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -7293,6 +7293,9 @@ var $;
         static href(next) {
             return next || process.argv.slice(2).join(' ');
         }
+        static href_normal() {
+            return this.link({});
+        }
         static dict(next) {
             if (next !== void 0)
                 this.href(this.make_link(next));
@@ -7355,6 +7358,9 @@ var $;
     __decorate([
         $.$mol_mem
     ], $mol_state_arg, "href", null);
+    __decorate([
+        $.$mol_mem
+    ], $mol_state_arg, "href_normal", null);
     __decorate([
         $.$mol_mem
     ], $mol_state_arg, "dict", null);
@@ -8242,7 +8248,7 @@ var $;
                 return new URL(this.uri(), base);
             }
             current() {
-                const base = this.$.$mol_state_arg.href();
+                const base = this.$.$mol_state_arg.href_normal();
                 const target = this.uri_native().toString();
                 if (base === target)
                     return true;
