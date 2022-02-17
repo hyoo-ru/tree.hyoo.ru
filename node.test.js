2d5e8ea10c35f6fefb9762c69ed34e9f7a66ab05
nin-jin <nin-jin@users.noreply.github.com>
2022-02-17 07:19:22 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -380,7 +380,7 @@ var $;
         }
         reap() { }
         promote() {
-            $mol_wire_auto?.track_next(this);
+            $mol_wire_auto()?.track_next(this);
         }
         up() { }
         down() { }
@@ -411,7 +411,11 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $.$mol_wire_auto = null;
+    let auto = null;
+    function $mol_wire_auto(next = auto) {
+        return auto = next;
+    }
+    $.$mol_wire_auto = $mol_wire_auto;
     $.$mol_wire_affected = [];
 })($ || ($ = {}));
 //mol/wire/wire.ts
@@ -532,8 +536,8 @@ var $;
         }
         track_on() {
             this.cursor = this.pub_from;
-            const sub = $mol_wire_auto;
-            $mol_wire_auto = this;
+            const sub = $mol_wire_auto();
+            $mol_wire_auto(this);
             return sub;
         }
         promote() {
@@ -575,7 +579,7 @@ var $;
             return pub;
         }
         track_off(sub) {
-            $mol_wire_auto = sub;
+            $mol_wire_auto(sub);
             if (this.cursor < 0) {
                 $mol_fail(new Error('End of non begun sub'));
             }
@@ -888,7 +892,7 @@ var $;
         host;
         task;
         static temp(host, task, ...args) {
-            const existen = $mol_wire_auto?.track_next();
+            const existen = $mol_wire_auto()?.track_next();
             reuse: if (existen) {
                 if (!(existen instanceof $mol_wire_fiber))
                     break reuse;
@@ -16062,7 +16066,7 @@ var $;
                 this.watch()?.();
             }
             finally {
-                for (const pub of $mol_wire_auto.pub_list) {
+                for (const pub of $mol_wire_auto().pub_list) {
                     if (pub instanceof $mol_wire_fiber) {
                         this.track(pub);
                     }
