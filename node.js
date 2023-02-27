a6c9d0c4caa265d72d9f1adda114dfcf06b136c2
nin-jin <nin-jin@users.noreply.github.com>
2023-02-27 07:18:56 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -9064,7 +9064,6 @@ var $;
                 justifyContent: 'space-between',
                 flex: 'none',
                 margin: 0,
-                overflow: 'hidden',
                 background: {
                     color: $mol_theme.card,
                 },
