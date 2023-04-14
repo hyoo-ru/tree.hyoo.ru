34e8a341736eee807aecb86f71a6ef393b13d39d
nin-jin <nin-jin@users.noreply.github.com>
2023-04-14 07:16:13 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1783,9 +1783,9 @@ declare namespace $ {
         readonly insertion: string;
         readonly deletion: string;
         readonly link: string;
-        readonly uri: string;
-        content: string;
         readonly marker: string;
+        readonly uri: string;
+        readonly content: string;
     }>;
 }
 
