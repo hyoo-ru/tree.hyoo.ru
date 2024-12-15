538c1872857466e06752f8c4c03425e7397e798e
nin-jin <nin-jin@users.noreply.github.com>
2024-12-15 07:11:27 +0000

    Deploying to gh-pages from @ hyoo-ru/tree.hyoo.ru@43f1e1f883a25f7ea2b2b617d14171f54acad8ee 🚀

--- web.d.ts
+++ web.d.ts
@@ -1807,7 +1807,7 @@ declare namespace $ {
 declare namespace $ {
     class $mol_syntax2<Lexems extends {
         [name: string]: RegExp;
-    }> {
+    } = {}> {
         lexems: Lexems;
         constructor(lexems: Lexems);
         rules: Array<{
